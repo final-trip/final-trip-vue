6a9fbe201327b2d46aa5f4c79f0e58856e9b721e
조은서 <eundeok9@gmail.com>
2023-11-09 16:11:19 +0900

    처음 설정

---
+++ src/components/layout/TheFooter.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer>
+		<p>풋터입니다</p>
+	</footer>
+</template>
+
+<style scoped>
+    footer{border-top:1px solid #35495e; text-align:center; font-size:16px; color:#41b883; margin:100px 0 0 0;}
+</style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <!-- <div>
+        <p>정복한 산</p>
+        
+    </div> -->
+<div class="head">
+
+    <router-link to="">정복한 산  </router-link> |
+    <router-link to="">랭킹</router-link>   
+    <router-link to="">기록</router-link>  
+
+</div>
+
+</template>
+
+<style scoped>
+.head{
+    margin-left: 333px;
+    color: aquamarine;
+}
+</style>
